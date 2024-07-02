19188c969c57bf58247a39a507f9e0d04494576b
zaib2023302 <zaib.un-nisa@soton.ac.uk>
2024-07-02 14:36:58 +0500

    Add astronaut script and data to work on

---
+++ my code v2.py
@@ -0,0 +1,61 @@
+# https://data.nasa.gov/resource/eva.json
+data_f = open('C:/Users/zun1g23/Downloads/astronaut-data-analysis-not-so-good-main/astronaut-data-analysis-not-so-good-main/data.json', 'r')
+data_t = open('C:/Users/zun1g23/Downloads/astronaut-data-analysis-not-so-good-main/astronaut-data-analysis-not-so-good-main/data.csv','w')
+g_file = 'myplot.png'
+
+fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
+
+data=[]
+import json
+
+for i in range(374):
+    line=data_f.readline()
+    print(line)
+    data.append(json.loads(line[1:-1]))
+#data.pop(0)
+## Comment out this bit if you don't want the spreadsheet
+import csv
+
+w=csv.writer(data_t)
+
+import datetime as dt
+
+time = []
+date =[]
+
+j=0
+for i in data:
+    print(data[j])
+    # and this bit
+    w.writerow(data[j].values())
+    if 'duration' in data[j].keys():
+        tt=data[j]['duration']
+        if tt == '':
+            pass
+        else:
+            t=dt.datetime.strptime(tt,'%H:%M')
+            ttt = dt.timedelta(hours=t.hour, minutes=t.minute, seconds=t.second).total_seconds()/(60*60)
+            print(t,ttt)
+            time.append(ttt)
+            if 'date' in data[j].keys():
+                date.append(dt.datetime.strptime(data[j]['date'][0:10], '%Y-%m-%d'))
+                #date.append(data[j]['date'][0:10])
+
+            else:
+                time.pop(0)
+    j+=1
+
+t=[0]
+for i in time:
+    t.append(t[-1]+i)
+
+date,time = zip(*sorted(zip(date, time)))
+
+import matplotlib.pyplot as plt
+
+plt.plot(date,t[1:], 'ko-')
+plt.xlabel('Year')
+plt.ylabel('Total time spent in space to date (hours)')
+plt.tight_layout()
+plt.savefig(g_file)
+plt.show()
